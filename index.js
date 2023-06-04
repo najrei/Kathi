fc88a99be8b84751ff78798f2b5aad6b0a593bc2
Jan Reimer <reimer-jan@gmx.de>
2023-06-04 15:02:54 +0200

    the webseite will be reachabl in you network

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const ipadress = 'ipadress';
 
 app.get('/', function (req,res) {
    res.sendFile('index.html', {root:__dirname});
@@ -11,7 +12,7 @@ app.get('/quiza', function (req, res){
 })
 
 app.get('/quizb', function (req, res) {
-   res.sendFile('kuche2.html', {root:__dirname});
+   res.sendFile('kueche2.html', {root:__dirname});
 })
 
 app.get('/quizc', function (req, res) {
@@ -22,6 +23,6 @@ app.get('/quizd', function (req, res) {
    res.sendFile('Schlafzimmer.html', {root:__dirname});
 } )
 
-app.listen(port, () => {
+app.listen(port, ipadress ,() => {
    console.log(`Now listening on port ${port}`);
 });
